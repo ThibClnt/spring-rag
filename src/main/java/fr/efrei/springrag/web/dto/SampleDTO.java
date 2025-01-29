749097ce8e7bdfd61c9e4a839919f3ade690034a
ThibClnt <thibaut.colnot@efrei.net>
2025-01-29 09:22:48 +0100

    #18 Add integration tests

---
+++ src/main/java/fr/efrei/springrag/web/dto/SampleDTO.java
@@ -0,0 +1,4 @@
+package fr.efrei.springrag.web.dto;
+
+public record SampleDTO(String value) {
+}
--- src/main/java/fr/efrei/springrag/web/rest/SampleResource.java
+++ src/main/java/fr/efrei/springrag/web/rest/SampleResource.java
@@ -1,5 +1,6 @@
 package fr.efrei.springrag.web.rest;
 
+import fr.efrei.springrag.web.dto.SampleDTO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,5 +11,10 @@ public class SampleResource {
     @GetMapping("/samples/{value}")
     public String hello(@PathVariable(value = "value", required = false) String value) {
         return "Hello " + value + "!";
+    }
+
+    @GetMapping("/samples/dto/{value}")
+    public SampleDTO helloDTO(@PathVariable(value = "value", required = false) String value) {
+        return new SampleDTO("Hello " + value + "!");
     }
 }
