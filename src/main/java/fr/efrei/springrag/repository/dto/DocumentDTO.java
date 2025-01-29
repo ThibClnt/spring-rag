f9e65f6dcd7a27f638359ad498acd2df6bc963d0
ThibClnt <thibaut.colnot@efrei.net>
2025-01-29 11:26:49 +0100

    #22 Added rag

---
+++ src/main/java/fr/efrei/springrag/repository/dto/DocumentDTO.java
@@ -0,0 +1,4 @@
+package fr.efrei.springrag.repository.dto;
+
+public record DocumentDTO(String title, String content) {
+}
--- src/main/java/fr/efrei/springrag/web/dto/Document.java
+++ src/main/java/fr/efrei/springrag/web/dto/Document.java
@@ -1,4 +1,4 @@
 package fr.efrei.springrag.web.dto;
 
-public record Document(Long id, String title) {
+public record Document(Long id, String title, String description, String author, String publisher, String publishedDate, String contentType, String content) {
 }
